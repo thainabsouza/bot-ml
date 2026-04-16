c7be06ada88793ef6b9d51a7b601363d1a3c5a4f
thainabsouza <thainasouza.sud@gmail.com>
2026-04-17 00:05:23 +0200

    ajustes no app e gitignore

--- src/app.js
+++ src/app.js
@@ -3,27 +3,30 @@ const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 
-const { executarBot } = require("./services/bot");
 const { loop } = require("./services/bot");
 
 const app = express();
 app.use(express.json());
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("Mongo conectado");
-
-    // 🚀 inicia o bot só depois do banco conectar
-    //executarBot();
-    loop();
-  })
-  .catch((err) => console.error("Erro Mongo:", err));
-
 app.use("/", routes);
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, "0.0.0.0", () => {
-  console.log("Servidor rodando na porta", PORT);
-});
+async function start() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Mongo conectado");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+
+      // inicia bot depois que tudo estiver OK
+      loop();
+    });
+  } catch (err) {
+    console.error("Erro inicializando app:", err);
+    process.exit(1);
+  }
+}
+
+start();
